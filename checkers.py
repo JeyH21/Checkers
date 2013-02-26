2091edcc1d256cf9dc480b169066520787460589
Dima <d954mas@mail.ru>
2013-02-27 00:28:45 +0400

    Can Eat
    Can Eat ,there is no damki

--- checkers.py
+++ checkers.py
@@ -1,4 +1,5 @@
 import os
+damka=0
 stroka=" +"+8*"--+"
 mas=[]  #масив одной строки поля
 poleigri=[] #массив клеток поля
@@ -14,6 +15,25 @@ for i in range(8):#генерация массива с клетками
        else:
            mas.append("  ")
     poleigri.append(mas)
+def Eatme(st):#Нужно кушать
+    eat=0
+    if poleigri[8-int(st[1])][ord(st[0])-65]=="бе":
+        rev="че"   #rev хранит противоположный цвет нашей шашки
+    else:
+        rev="бе"
+    pass
+    if ((ord(st[0])-ord(st[2]) +int(st[1])-int(st[3]))%2==0)and (ord(st[0])!=ord(st[2])and int(st[1])!=int(st[3]))  :#Ecли статок ноль ,то ход лежит на диагоналях
+        d1=int(ord(st[2])+(ord(st[0])-ord(st[2]))/2)
+        d2=int(int(st[3])+(int(st[1])-int(st[3]))/2)
+        if damka==0: #если простая шашка 
+           if poleigri[8-d2][d1-65]==rev:
+               eat=1
+##              poleigri[8-d2] [d1-65]="  "
+##              poleigri[8-int(st[3])][ord(st[2])-65]=poleigri[8-int(st[1])][ord(st[0])-65]
+##              poleigri[8-int(st[1])][ord(st[0])-65]="  ")
+        else:  #если ДАМКА, пока не работает
+            pass  
+    return eat
 def pole():   #функция для генерации поля
     os.system('cls')
     for i in range(8):
@@ -27,33 +47,59 @@ def pole():   #функция для генерации поля
         print(str(9-i)+"\n"+stroka) #нумерация сбоку 2
     for i in range(8):
         print("  "+chr(ord("A")+i),end="") #нумерация снизу
-def xod():
+    print()
+def xod(KolHod):
+    if KolHod%2==1:
+        igrok="белые"
+    else:
+        igrok="черные"
     while 1:    
         while 1:
-            st=input("\nход н игрока ") #ход шашки
+            st=input("Ходят "+igrok+"\n" ) #ход шашки
             st=st.strip() #убираем лишние пробелы в начале строки и в конце
             #Проверяем правильность введенных данных
             try:
                 if (1<=int(st[1])<=8 and 1<=int(st[3])<=8)and ((65<=ord(st[0])<=72 and 65<=ord(st[2])<=72))and len(st)<5 :
                     break
                 else:
-                    print("Нет такой позиции на доске")
+                    print("Нет такой позиции на доскеSAADSADFDSAFDSAFDSA")
             except (IndexError , ValueError) :
                     print("Вы ничего не ввели,или ввели не правильно")
-        if (int(st[1])==int(st[3])-1)and(ord(st[0])==ord(st[2])+1 or ord(st[0])==ord(st[2])-1) and poleigri[8-int(st[3])][ord(st[2])-65]=="  "and poleigri[8-int(st[1])][ord(st[0])-65]=="бе": #если ход по диагонали и клетка свободна то ходим
-            poleigri[8-int(st[3])][ord(st[2])-65]=poleigri[8-int(st[1])][ord(st[0])-65]
-            poleigri[8-int(st[1])][ord(st[0])-65]="  "
-            break
+            #Проверяем возможность хода отдельно для белых и черных
+        if KolHod%2==1 and poleigri[8-int(st[1])][ord(st[0])-65]=="бе":
+            if(int(st[1])==int(st[3])-1)and(ord(st[0])==ord(st[2])+1 or ord(st[0])==ord(st[2])-1) and poleigri[8-int(st[3])][ord(st[2])-65]=="  "  :#если ход по диагонали и клетка свободна то ходим
+                poleigri[8-int(st[3])][ord(st[2])-65]=poleigri[8-int(st[1])][ord(st[0])-65]
+                poleigri[8-int(st[1])][ord(st[0])-65]="  "
+                break
+            eat=Eatme(st)
+            if eat==1:
+                poleigri[int(8-int(st[3])-(int(st[1])-int(st[3]))/2)] [int(ord(st[2])+(ord(st[0])-ord(st[2]))/2-65)]="  "
+                poleigri[8-int(st[3])][ord(st[2])-65]=poleigri[8-int(st[1])][ord(st[0])-65]
+                poleigri[8-int(st[1])][ord(st[0])-65]="  "
+                break
+            else:
+                print("Данный ход невозможен")
+        elif KolHod%2==0 and poleigri[8-int(st[1])][ord(st[0])-65]=="че":
+            if(int(st[1])==int(st[3])+1)and(ord(st[0])==ord(st[2])+1 or ord(st[0])==ord(st[2])-1) and poleigri[8-int(st[3])][ord(st[2])-65]=="  ":#если ход по диагонали и клетка свободна то ходим
+                poleigri[8-int(st[3])][ord(st[2])-65]=poleigri[8-int(st[1])][ord(st[0])-65]
+                poleigri[8-int(st[1])][ord(st[0])-65]="  "
+                break
+            eat=Eatme(st)
+            if eat==1:
+                poleigri[int(8-int(st[3])-(int(st[1])-int(st[3]))/2)] [int(ord(st[2])+(ord(st[0])-ord(st[2]))/2-65)]="  "
+                poleigri[8-int(st[3])][ord(st[2])-65]=poleigri[8-int(st[1])][ord(st[0])-65]
+                poleigri[8-int(st[1])][ord(st[0])-65]="  "
+                break
+            else:
+                print("Данный ход невозможен")
         else:
-            print("Данный ход не возможен")
+            print("Вы можете играть только своими фигурами")
+    return KolHod
+n=1
 while 1:
     pole()
-    xod()
-    pole()        
-    xod()
-    pole()
-
-
+    xod(n)
+    n=n+1
+   
         
-    
   
